eb3fc37daff132ba6ef376633f52cefb2174946d
Inna-Badiul <inna.badiul@gmail.com>
2017-01-18 01:48:55 +0100

    added basic stucture of landing page, create responsive header, added video background, added images, working with about us section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".hamburger-button").click(function () {
+        $(".menu-items-wrapper").show();
+        $(".close-button").show();
+        $(".nav-menu").addClass("nav-open");
+    });
+
+    $(".close-button").click(function () {
+        $(".menu-items-wrapper").hide();
+        $(".nav-menu").removeClass("nav-open");
+        $(".close-button").hide();
+    });
+    
+});
