5833af6f765780910d4929adfb8bbb332c63b8b7
Inna-Badiul <inna.badiul@gmail.com>
2017-01-23 19:48:45 +0100

    changed the structure of the code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var hamburgerButton = document.querySelector(".hamburger-button"),
         listItem = document.querySelectorAll(".list-item"),
-        navMenu = document.querySelector(".nav-menu");
+        navMenu = document.querySelector(".nav-menu"),
+        isOpen = false;
 
-    hamburgerButton.addEventListener("click", function () {
-        navMenu.classList.add("nav-open");
-    });
-
-    listItem.forEach(function (item) {
+    function closeMenu(item) {
         item.addEventListener("click", function () {
             navMenu.classList.remove("nav-open");
         });
-    });
+    }
+
+    function navOpen() {
+        navMenu.classList.add("nav-open");
+        isOpen = true;
+    }
+    function toggleMenu() {
+        if (isOpen === false) {
+            navOpen();
+        } else {
+            navMenu.classList.remove("nav-open");
+            isOpen = false;
+        }
+    }
+    hamburgerButton.addEventListener("click", toggleMenu);
+    listItem.forEach(closeMenu);
 });
